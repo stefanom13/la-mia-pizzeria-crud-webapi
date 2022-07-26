9c546f5028b655df1570cc655321041bacb36df4
stefanom13 <stefanomancuso13@gmail.com>
2022-07-26 14:36:37 +0200

    Dati stampati in json

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
