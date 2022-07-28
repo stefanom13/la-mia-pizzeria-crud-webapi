19d09f1e1c3c880957bea3c9cebf2478b6af2c58
stefanom13 <stefanomancuso13@gmail.com>
2022-07-28 14:13:23 +0200

    creato controller api message

---
+++ Controllers/Api/MessageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_mvc_refactoring.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+    }
+}
