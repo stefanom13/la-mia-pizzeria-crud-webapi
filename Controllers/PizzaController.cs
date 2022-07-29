c7312524ddffaca5718eb341512a000e9956d9d3
stefanom13 <stefanomancuso13@gmail.com>
2022-07-29 15:54:23 +0200

    Index eseguita

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -10,217 +10,225 @@ namespace la_mia_pizzeria_mvc_refactoring.Controllers
 {
     public class PizzaController : Controller
     {
-        private DbPizzaRepository? PizzaRepository;
+        private DbPizzaRepository pizzaRepository;
 
         public PizzaController()
         {
-            this.PizzaRepository = new DbPizzaRepository();
+            this.pizzaRepository = new DbPizzaRepository();
         }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> posts = pizzaRepository.GetList();
+            return View("Index", posts);
+        }
+
 
         // GET: HomeController1
-        public ActionResult Index()
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                IQueryable<Pizza> listPizza = db.Pizze.Include(piz => piz.Categorie);
-                // List<Pizza> listPizza = db.Pizzas.OrderBy(pizza => pizza.Id).ToList<Pizza>();
-                if (listPizza == null)
-                {
-                    return NotFound("Pizze non presenti");
-                }
-                return View("Index", listPizza.ToList());
-            }
-        }
+        //public ActionResult Index()
+        //{
+           // using (PizzaContext db = new PizzaContext())
+           // {
+             //   IQueryable<Pizza> listPizza = db.Pizze.Include(piz => piz.Categorie);
+               // // List<Pizza> listPizza = db.Pizzas.OrderBy(pizza => pizza.Id).ToList<Pizza>();
+               // if (listPizza == null)
+               // {
+                  ///  return NotFound("Pizze non presenti");
+               // }
+                //return View("Index", listPizza.ToList());
+            //}
+        //}
 
         // GET: HomeController1/Details/5
-        public ActionResult Details(int id)
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                //Pizza pizza = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
-                Pizza pizza = db.Pizze.Where(pizza => pizza.Id == id).Include(cat => cat.Categorie).FirstOrDefault();
+        //public ActionResult Details(int id)
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        //Pizza pizza = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+        //        Pizza pizza = db.Pizze.Where(pizza => pizza.Id == id).Include(cat => cat.Categorie).FirstOrDefault();
 
-                if (pizza == null)
-                {
-                    return NotFound("Pizza non trovata");
-                }
-                else
-                {
-                    db.Entry(pizza).Collection("Ingredienti").Load();
-                    return View("Details", pizza);
-                }
-            }
-        }
+        //        if (pizza == null)
+        //        {
+        //            return NotFound("Pizza non trovata");
+        //        }
+        //        else
+        //        {
+        //            db.Entry(pizza).Collection("Ingredienti").Load();
+        //            return View("Details", pizza);
+        //        }
+        //    }
+        //}
 
-        // GET: HomeController1/Create
-        public ActionResult Create()
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                List<Categoria> categoria = db.Categorie.ToList();
-               // List<Ingredienti> ListaIngredieni = db.Ingrediente.ToList();
-                PizzaCategorie model = new PizzaCategorie();
+        //// GET: HomeController1/Create
+        //public ActionResult Create()
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        List<Categoria> categoria = db.Categorie.ToList();
+        //       // List<Ingredienti> ListaIngredieni = db.Ingrediente.ToList();
+        //        PizzaCategorie model = new PizzaCategorie();
 
-                model.Pizza = new Pizza();
-                model.Categorie = categoria;
-                model.ListaIngredienti = NuovoMetodo();
+        //        model.Pizza = new Pizza();
+        //        model.Categorie = categoria;
+        //        model.ListaIngredienti = NuovoMetodo();
                 
                 
-                return View(model);
-            }
-        }
+        //        return View(model);
+        //    }
+        //}
 
         
 
-        // POST: HomeController1/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(PizzaCategorie p)
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
+        //// POST: HomeController1/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(PizzaCategorie p)
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
 
 
-                if (!ModelState.IsValid)
-                {
-                    List<Categoria> categoria = db.Categorie.ToList();
-                    // List<Ingredienti> ListaIngredieni = db.Ingrediente.ToList();
+        //        if (!ModelState.IsValid)
+        //        {
+        //            List<Categoria> categoria = db.Categorie.ToList();
+        //            // List<Ingredienti> ListaIngredieni = db.Ingrediente.ToList();
 
 
-                    p.Pizza = new Pizza();
-                    p.Categorie = categoria;
-                    p.ListaIngredienti = NuovoMetodo();
+        //            p.Pizza = new Pizza();
+        //            p.Categorie = categoria;
+        //            p.ListaIngredienti = NuovoMetodo();
 
 
-                    return View("Create", p);
-                    /// p.Categorie = db.Categorie.ToList();
-                    // return View("Create", p);
-                }
-            }
-                using (PizzaContext db = new PizzaContext())
-                {
-                    Pizza newPizza = new Pizza();
-                    newPizza.NomePizza = p.Pizza.NomePizza;
-                    newPizza.Descrizione = p.Pizza.Descrizione;
-                    newPizza.Prezzo = p.Pizza.Prezzo;
-                    newPizza.PathImage = p.Pizza.PathImage;
-                    newPizza.CategoriaId = p.Pizza.CategoriaId;
-                    newPizza.Ingredienti = new List<Ingredienti>();
+        //            return View("Create", p);
+        //            /// p.Categorie = db.Categorie.ToList();
+        //            // return View("Create", p);
+        //        }
+        //    }
+        //        using (PizzaContext db = new PizzaContext())
+        //        {
+        //            Pizza newPizza = new Pizza();
+        //            newPizza.NomePizza = p.Pizza.NomePizza;
+        //            newPizza.Descrizione = p.Pizza.Descrizione;
+        //            newPizza.Prezzo = p.Pizza.Prezzo;
+        //            newPizza.PathImage = p.Pizza.PathImage;
+        //            newPizza.CategoriaId = p.Pizza.CategoriaId;
+        //            newPizza.Ingredienti = new List<Ingredienti>();
 
-                    if (p.SelezionaIngrediente != null)
-                    {
-                        foreach (string ingredient in p.SelezionaIngrediente)
-                        {
-                            int selectedIntTagId = Int32.Parse(ingredient);
+        //            if (p.SelezionaIngrediente != null)
+        //            {
+        //                foreach (string ingredient in p.SelezionaIngrediente)
+        //                {
+        //                    int selectedIntTagId = Int32.Parse(ingredient);
 
-                            Ingredienti ingrediente = db.Ingrediente.Where(p => p.Id == selectedIntTagId).FirstOrDefault();
+        //                    Ingredienti ingrediente = db.Ingrediente.Where(p => p.Id == selectedIntTagId).FirstOrDefault();
 
-                            newPizza.Ingredienti.Add(ingrediente);
-                        }
-                    }
-                    db.Pizze.Add(newPizza);
+        //                    newPizza.Ingredienti.Add(ingrediente);
+        //                }
+        //            }
+        //            db.Pizze.Add(newPizza);
 
-                    db.SaveChanges();
+        //            db.SaveChanges();
                    
-                }
-            return RedirectToAction("Index");
+        //        }
+        //    return RedirectToAction("Index");
 
-        }
-        static List<SelectListItem>? NuovoMetodo()
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                List<SelectListItem> ingredientiLista = new List<SelectListItem>();
-                List<Ingredienti> ingredients = db.Ingrediente.ToList();
+        //}
+        //static List<SelectListItem>? NuovoMetodo()
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        List<SelectListItem> ingredientiLista = new List<SelectListItem>();
+        //        List<Ingredienti> ingredients = db.Ingrediente.ToList();
 
-                foreach (Ingredienti ingrediente in ingredients)
-                {
-                    ingredientiLista.Add(new SelectListItem() { Text = ingrediente.NomeIngrediente, Value = ingrediente.Id.ToString() });
-                }
+        //        foreach (Ingredienti ingrediente in ingredients)
+        //        {
+        //            ingredientiLista.Add(new SelectListItem() { Text = ingrediente.NomeIngrediente, Value = ingrediente.Id.ToString() });
+        //        }
 
-                return ingredientiLista;
-            }
-        }
+        //        return ingredientiLista;
+        //    }
+        //}
 
-        // GET: HomeController1/Edit/5
-        public ActionResult Edit(int id)
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                Pizza pizzaEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
-                if (pizzaEdit == null)
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    PizzaCategorie model = new PizzaCategorie();
+        //// GET: HomeController1/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        Pizza pizzaEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+        //        if (pizzaEdit == null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            PizzaCategorie model = new PizzaCategorie();
 
-                    model.Pizza = pizzaEdit;
-                    model.Categorie = db.Categorie.ToList();
+        //            model.Pizza = pizzaEdit;
+        //            model.Categorie = db.Categorie.ToList();
 
-                    return View(model);
-                }
-            }
-        }
+        //            return View(model);
+        //        }
+        //    }
+        //}
 
-        // POST: HomeController1/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, PizzaCategorie p)
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                if (!ModelState.IsValid)
-                {
-                    p.Categorie = db.Categorie.ToList();
-                    return View("Edit", p);
-                }
+        //// POST: HomeController1/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, PizzaCategorie p)
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        if (!ModelState.IsValid)
+        //        {
+        //            p.Categorie = db.Categorie.ToList();
+        //            return View("Edit", p);
+        //        }
 
-                Pizza pizzaEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+        //        Pizza pizzaEdit = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
 
 
-                if (pizzaEdit != null)
-                {
-                    pizzaEdit.NomePizza = p.Pizza.NomePizza;
-                    pizzaEdit.Descrizione = p.Pizza.Descrizione;
-                    pizzaEdit.PathImage = p.Pizza.PathImage;
-                    pizzaEdit.Prezzo = p.Pizza.Prezzo;
-                    pizzaEdit.CategoriaId = p.Pizza.CategoriaId;
+        //        if (pizzaEdit != null)
+        //        {
+        //            pizzaEdit.NomePizza = p.Pizza.NomePizza;
+        //            pizzaEdit.Descrizione = p.Pizza.Descrizione;
+        //            pizzaEdit.PathImage = p.Pizza.PathImage;
+        //            pizzaEdit.Prezzo = p.Pizza.Prezzo;
+        //            pizzaEdit.CategoriaId = p.Pizza.CategoriaId;
 
-                    db.SaveChanges();
-                }
-                else
-                {
-                    return NotFound(View("Error"));
-                }
-            }
-            return RedirectToAction("Index");
-        }
+        //            db.SaveChanges();
+        //        }
+        //        else
+        //        {
+        //            return NotFound(View("Error"));
+        //        }
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
-        // GET: HomeController1/Delete/5
+        //// GET: HomeController1/Delete/5
       
-        // POST: HomeController1/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
-        {
-            using (PizzaContext db = new PizzaContext())
-            {
-                Pizza pizzaDelete = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
+        //// POST: HomeController1/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id)
+        //{
+        //    using (PizzaContext db = new PizzaContext())
+        //    {
+        //        Pizza pizzaDelete = db.Pizze.Where(pizza => pizza.Id == id).FirstOrDefault();
 
-                if (pizzaDelete == null)
-                {
+        //        if (pizzaDelete == null)
+        //        {
 
-                    return NotFound();
-                }
-                else
-                {
-                    db.Pizze.Remove(pizzaDelete);
-                    db.SaveChanges();
-                    return RedirectToAction("Index");
-                }
-            }
-        }
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            db.Pizze.Remove(pizzaDelete);
+        //            db.SaveChanges();
+        //            return RedirectToAction("Index");
+        //        }
+        //    }
+        //}
     }
 }
--- Models/Repositoris/DbPizzaRepository.cs
+++ Models/Repositoris/DbPizzaRepository.cs
@@ -1,9 +1,55 @@
-﻿namespace la_mia_pizzeria_mvc_refactoring.Models.Repositoris
+﻿using la_mia_pizzeria_mvc_refactoring.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_mvc_refactoring.Models.Repositoris
 {
-    internal class DbPizzaRepository
+    public class DbPizzaRepository
     {
-        public DbPizzaRepository()
+        public void Create(Pizza post, List<string> selectedTags)
         {
+            using (PizzaContext context = new PizzaContext())
+            {
+                post.Ingredienti = new List<Ingredienti>();
+                if (selectedTags != null)
+                {
+                    foreach (string selectedTagId in selectedTags)
+                    {
+                        int selectedIntTagId = int.Parse(selectedTagId);
+                        Ingredienti ingredienti = context.Ingrediente.Where(m => m.Id == selectedIntTagId).FirstOrDefault();
+                        post.Ingredienti.Add(ingredienti);
+                    }
+                }
+                context.Pizze.Add(post);
+                context.SaveChanges();
+            }
         }
+        public void Delete(Pizza post)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                context.Pizze.Remove(post);
+                context.SaveChanges();
+            }
+        }
+        public Pizza GetById(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza postFound = context.Pizze.Where(post => post.Id == id).Include(post =>
+                post.Categorie).Include(m => m.Ingredienti).FirstOrDefault();
+                return postFound;
+            }
+        }
+        public List<Pizza> GetList()
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                IQueryable<Pizza> posts = context.Pizze;
+                return posts.ToList();
+            }
+        }
+
+
     }
+
 }
